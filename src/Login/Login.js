85d7062fe93b45472a23e4d0b4703c9dccda8e76
Thaiza12 <87837910+Thaiza12@users.noreply.github.com>
2021-11-21 13:03:54 -0300

    Add files via upload

---
+++ src/Login/Login.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Login.css";
+
+
+export default function Login (){
+
+    function mostrarMensagem(){
+        alert("O nome do usuário e/ou senha está inválido.")
+    }
+
+
+    return(
+    <div>
+        <form className="form">
+            <div className="usuario">
+                <input type="texto" name="username" placeholder="Nome do usuário"/>
+            </div>
+            <div className="senha">
+                <input type="password" name="Senha"  placeholder="Senha"/>
+                </div>
+                <button className="botao" onClick={()=>mostrarMensagem()}>LOGIN</button>
+                <p className="esqueceu">Esqueceu sua senha? <a target="_blank" href="http://google.com">Clique aqui</a></p>
+        </form>
+    </div>
+    );
+}
